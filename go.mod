1f8a4faca6c816228f2d2e0b4573ee36d2d00686
Zunchy <zwaterbury1289@gmail.com>
2025-03-02 12:36:28 -0500

    Adding endpoints to add a logging request with some basline details and to purge records for a producer.

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module proxy
 
 go 1.24.0
 
+require github.com/jackc/pgx v3.6.2+incompatible
+
 require (
 	github.com/bytedance/sonic v1.12.9 // indirect
 	github.com/bytedance/sonic/loader v0.2.3 // indirect
@@ -27,8 +29,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.14.0 // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
--- webhook_logger.go
+++ webhook_logger.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/pgtype"
+	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -15,19 +24,41 @@ var db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.
 	SingularTable: true,
 }})
 
+var logger *zap.Logger
+
 type MasterWebhookServer struct {
 	WebhookServerUrl string `json:"webhookServerUrl"`
 }
 
 type Producer struct {
-	Id  uint   `json:"id"`
-	URL string `json:"url"`
+	Id           uint      `json:"id"`
+	URL          string    `json:"url"`
+	LastAccessed time.Time `json:"lastAccessed"`
+}
+
+type RequestDetails struct {
+	Id             uint         `json:"id"`
+	ProducerId     uint         `json:"producerId"`
+	URL            string       `json:"url"`
+	Timestamp      time.Time    `json:"timestamp"`
+	HttpMethod     string       `json:"httpMethod"`
+	Headers        pgtype.JSONB `json:"headers"`
+	ResponseStatus uint         `json:"responseStatus"`
+	ResponseTime   float32      `json:"responseTime"`
 }
 
 func main() {
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
 	router := gin.Default()
 	router.GET("/masterWebhookServer", getMasterWebhookServer)
 	router.POST("/producer", registerProducer)
+	router.POST("/request", logRequest)
+	router.DELETE("/purge", purgeRequestRecords)
 
 	router.Run("localhost:8080")
 }
@@ -50,9 +81,92 @@ func registerProducer(c *gin.Context) {
 	result := db.First(&existingProducer, "lower(url) = lower(?)", producer.URL)
 
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		producer.LastAccessed = time.Now()
 		db.Create(&producer)
 		c.JSON(http.StatusCreated, producer)
 	} else {
 		c.JSON(http.StatusOK, existingProducer)
 	}
+}
+
+func logRequest(c *gin.Context) {
+	var requestDetails RequestDetails
+
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "Unable to read body")
+		return
+	}
+
+	var requestBody map[string]interface{}
+	err = json.Unmarshal(body, &requestBody)
+	if err != nil {
+		panic(err)
+	}
+
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	if c.BindJSON(&requestDetails) != nil {
+		return
+	}
+
+	requestDetails.Headers.Set(requestBody["headers"])
+
+	result := db.Create(&requestDetails)
+
+	if result.Error != nil {
+		logger.Error(result.Error.Error())
+		c.JSON(http.StatusInternalServerError, "Record Creation Failed!")
+	}
+
+	c.JSON(http.StatusCreated, requestDetails)
+}
+
+func purgeRequestRecords(c *gin.Context) {
+	daysToKeep, err := strconv.Atoi(c.Query("DaysToKeep"))
+	if err != nil {
+		logger.Error(err.Error())
+		c.JSON(http.StatusInternalServerError, false)
+	}
+
+	producerId, err := strconv.Atoi(c.Query("ProducerId"))
+	if err != nil {
+		logger.Error(err.Error())
+		c.JSON(http.StatusInternalServerError, false)
+	}
+
+	now := time.Now()
+	detailsToKeepDate := now.AddDate(0, 0, -daysToKeep)
+
+	result := db.Delete(&RequestDetails{}, "timestamp < ? AND producer_Id = ?", detailsToKeepDate, producerId)
+
+	if result.Error != nil {
+		logger.Error(result.Error.Error())
+		c.JSON(http.StatusInternalServerError, false)
+	}
+
+	c.JSON(http.StatusOK, true)
+}
+
+func ExtractHeaderJsonData(details RequestDetails) map[string]interface{} {
+	var data map[string]interface{}
+	if err := json.Unmarshal(details.Headers.Bytes, &data); err != nil {
+		logger.Fatal("Failed to unmarshal JSONB bytes:")
+	}
+
+	if str, ok := data["Bytes"].(string); ok {
+		decoded, err := base64.StdEncoding.DecodeString(str)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if err := json.Unmarshal([]byte(string(decoded[:])), &data); err != nil {
+			logger.Fatal("Failed to unmarshal JSONB string:")
+		}
+	}
+
+	delete(data, "Bytes")
+	delete(data, "Status")
+
+	return data
 }
